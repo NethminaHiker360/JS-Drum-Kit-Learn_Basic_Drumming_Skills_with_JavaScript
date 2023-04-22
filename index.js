771d02672a2cfea5ab466246c8f28d54e92f9d42
Ravindu Nethmina <nethmina.kar@gmail.com>
2023-04-22 12:27:22 +0530

    complete

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
-const kits=["crash","kick","Snare","tom"];
+const kits = ["crash", "kick", "snare", "tom"];
 
-const containerEl = document.querySelector("container");
+const containerEl = document.querySelector(".container");
 
-kits.forEach((kit)=>{
-    const btnEl=document.createElement("button");
+kits.forEach((kit) => {
+    const btnEl = document.createElement("button");
     btnEl.classList.add("btn");
+    btnEl.innerText = kit;
+    btnEl.style.backgroundImage = "url(pictures/" + kit + ".jpg)";
     containerEl.appendChild(btnEl);
-})
+    const audioEl = document.createElement("audio");
+    audioEl.src = "sounds/" + kit + ".wav";
+    containerEl.appendChild(audioEl);
+    btnEl.addEventListener("click", () => {
+        audioEl.play();
+    });
+    window.addEventListener("keydown", (event) => {
+        if (event.key === kit.slice(0, 1)) {
+            audioEl.play();
+            btnEl.style.transform = "scale(.9)";
+            setTimeout(() => {
+                btnEl.style.transform = "scale(1)";
+            }, 100);
+        }
+    });
+});
